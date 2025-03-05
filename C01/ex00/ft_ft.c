a58b000bbe86c45216bbbf0d1e6d78bf2f2a6c88
Motheraudio <alvaro.camposmndz@gmail.com>
2025-03-05 17:51:59 +0100

    Added C01 Ex00 and Ex01

---
+++ C01/ex00/ft_ft.c
@@ -0,0 +1,20 @@
+
+void	ft_ft(int *nbr)
+{
+ *nbr = 42;
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	int	x;
+	int	*ptr;
+
+	x = 0;
+	ptr = &x;
+	printf("%d\n", x);
+	ft_ft(ptr);
+	printf("%d\n", x);
+}
+*/
---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,26 @@
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+/*
+#include <stdio.h>
+
+int	main()
+{
+	int	number = 123;
+	int	*ptr1 = &number;
+	int	**ptr2 = &ptr1;
+ 	int	***ptr3 = &ptr2;
+ 	int	****ptr4 = &ptr3;
+	int *****ptr5 = &ptr4;
+ 	int	******ptr6 = &ptr5;
+ 	int	*******ptr7 = &ptr6;
+ 	int	********ptr8 = &ptr7;
+ 	int	*********ptr9 = &ptr8;
+	
+	printf("before: %d\n", number);
+	ft_ultimate_ft(ptr9);
+	printf("after: %d\n", number);
+
+}
+*/
